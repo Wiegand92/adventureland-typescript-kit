fed0218378156b480c110454db5c0d74960ec845
Wiegand92 <wiegand.alex@gmail.com>
2022-08-30 14:47:00 -0400

    add ts-node, and convert watcher to ts

--- watcher.ts
+++ watcher.ts
@@ -9,10 +9,10 @@ console.log('Watcher Initializing...');
 
 // Global State //
 let initialized = false;
-let rollupWatcher;
+let rollupWatcher: any;
 const watcher = chokidar.watch(path.join(__dirname, './src/uploads'));
 
-watcher.on('add', file => {
+watcher.on('add', (file: any) => {
   if (initialized) {
     console.log(`${file} has been added!`);
     if (rollupWatcher) {
@@ -28,16 +28,16 @@ watcher.on('ready', () => {
   startRollup();
 });
 
-watcher.on('change', file => {
+watcher.on('change', (file: any) => {
   console.log(`${file} has been changed`);
 });
 
 function startRollup() {
   return loadConfigFile(path.resolve(__dirname, 'rollup.config.ts'), {
     configPlugin: 'typescript',
-  }).then(async ({ options, warnings }) => {
+  }).then(async ({ options, warnings }: any) => {
     rollupWatcher = rollup.watch(options);
-    rollupWatcher.on('event', ({ result }) => {
+    rollupWatcher.on('event', ({ result }: any) => {
       if (result) {
         result.close();
       }
